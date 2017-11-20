859390924536f77458beb8968b0cebfa948e7c53
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2017-11-21 06:33:23 +1100

    Progress with migrating CoreData related code to IFACoreData.

---
+++ IFACoreUI/IFACoreUITests/classes/Dummy.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy.swift
+//  IFACoreUITests
+//
+//  Created by Marcelo Schroeder on 21/11/17.
+//  Copyright © 2017 InfoAccent Pty Ltd. All rights reserved.
+//
+
+/**
+ * Workaround for problem described here: https://github.com/CocoaPods/CocoaPods/issues/7170
+ */
+
+import Foundation
+import Accelerate
+import CoreLocation
+import GLKit
+import MapKit
